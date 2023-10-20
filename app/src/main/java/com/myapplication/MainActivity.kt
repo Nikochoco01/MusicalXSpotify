718d5651d1bdc7aa3558c669755f1089a7aee22c
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-10-20 13:38:39 +0200

    update of main class of app

--- app/src/main/java/com/myapplication/MainActivity.kt
+++ app/src/main/java/com/myapplication/MainActivity.kt
@@ -1,47 +1,19 @@
 package com.myapplication
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalContext
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.debugInspectorInfo
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.myapplication.dataSource.SpotifyClient
-import com.myapplication.model.accessToken.ApiAccess
 import com.myapplication.ui.theme.MusicalXSpotifyTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val loginViewModel : LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -49,114 +21,12 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF6650a4)
                 ) {
-                    Greeting()
-                    executeCall(this)
+                    MusicalApp(
+                        loginViewModel
+                    )
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ){
-        Text(
-            modifier = Modifier
-                .width(279.dp)
-                .height(16.dp),
-            text = "Musical X Spotify",
-            style = TextStyle(
-                fontSize = 48.sp,
-                lineHeight = 16.sp,
-//                fontFamily = FontFamily(Font(R.font.nanum brush script)),
-                fontWeight = FontWeight(400),
-                color = Color(0xFFFFB597),
-            )
-        )
-
-        Column(
-            Modifier
-                .width(360.dp)
-                .height(264.dp)
-                .padding(start = 16.dp, end = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(48.dp, Alignment.Top),
-            horizontalAlignment = Alignment.Start,
-        ) {
-            // Child views.
-//            TextField(
-//                Modifier
-//                    .width(328.dp)
-//                    .height(56.dp)
-//                    .background(color = Color(0xFF2F2826), shape = RoundedCornerShape(size = 4.dp),
-//                value = "mail", onValueChange = "mail"){}
-        }
-
-        Row(
-            Modifier
-                .width(360.dp)
-                .height(40.dp)
-                .padding(start = 16.dp, end = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(64.dp, Alignment.CenterHorizontally),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Button(onClick = { /*TODO*/ }) {
-                Modifier
-                    .padding(0.dp)
-                    .width(124.dp)
-                    .height(40.dp)
-                    .background(
-                        color = Color(0xFFFFB597),
-                        shape = RoundedCornerShape(size = 100.dp)
-                    );
-                Text(
-                    text = "Connection",
-                    // M3/label/large
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        lineHeight = 20.sp,
-                        //fontFamily = FontFamily(Font(R.font.roboto)),
-                        fontWeight = FontWeight(500),
-                        color = Color(0xFF581D00),
-                        textAlign = TextAlign.Center,
-                        letterSpacing = 0.1.sp,
-                    )
-                )
-            }
-
-            Button(onClick = { /*TODO*/ }) {
-                Modifier
-                    .border(
-                        width = 1.dp,
-                        color = Color(0xFFA08D86),
-                        shape = RoundedCornerShape(size = 100.dp)
-                    )
-                    .width(124.dp)
-                    .height(40.dp)
-                    .padding(start = 16.dp, top = 10.dp, end = 24.dp, bottom = 10.dp);
-                //Image(
-                  //  painter = painterResource(id = R.drawable.icon),
-                   // contentDescription = "image description",
-                   // contentScale = ContentScale.None
-                //);
-                Text(
-                    text = "Subscribe",
-                    // M3/label/large
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        lineHeight = 20.sp,
-                        //fontFamily = FontFamily(Font(R.font.roboto)),
-                        fontWeight = FontWeight(500),
-                        color = Color(0xFFFFB597),
-                        textAlign = TextAlign.Center,
-                        letterSpacing = 0.1.sp,
-                    )
-                )
             }
         }
     }
@@ -166,40 +36,11 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MusicalXSpotifyTheme {
-        Greeting()
-    }
-}
-
-private fun executeCall(context: Context) {
-    val credential: String = ApiAccess().grantType
-    val clientId: String = ApiAccess().clientId
-    val clientSecret: String = ApiAccess().clientSecret
-    GlobalScope.launch(Dispatchers.Main) {
-        try {
-            val response = SpotifyClient.apiServiceAccessToken.getAccessToken(credential, clientId, clientSecret)
-
-            if (response.isSuccessful && response.body() != null) {
-                val content = response.body()
-                Log.d("Resp token", content?.accessToken.toString())
-                Log.d("Resp token type", content?.tokenType.toString())
-                Log.d("Resp token expires", content?.expiresIn.toString())
-            } else {
-                Log.e("Error resp message", response.message())
-                Log.e("Error resp code", response.code().toString())
-                Toast.makeText(
-                    context,
-                    "Error Occurred: ${response.message()}",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-
-        } catch (e: Exception) {
-            Log.e("Error exception", "error: ${e.message}")
-            Toast.makeText(
-                context,
-                "Error Occurred: ${e.message}",
-                Toast.LENGTH_LONG
-            ).show()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(0xFF6650a4)
+        ) {
+//            MusicalApp(loginViewModel = )
         }
     }
 }
---
+++ app/src/main/java/com/myapplication/MusicalApp.kt
@@ -0,0 +1,167 @@
+package com.myapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.GraphicEq
+import androidx.compose.material.icons.filled.LibraryMusic
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.myapplication.model.accessToken.AccessToken
+import com.myapplication.ui.PlaylistView
+import com.myapplication.ui.ReaderView
+import com.myapplication.ui.SettingsView
+
+@Composable
+fun MusicalApp(
+    loginViewModel: LoginViewModel,
+    navigateToDetail: (Long) -> Unit = {}
+) {
+    val token by loginViewModel.spotifyTokenLiveData.observeAsState()
+
+//    LaunchedEffect(Unit){
+//        loginViewModel.fetchSpotifyToken()
+//    }
+    MusicalAppContent(
+        navigateToDetail = navigateToDetail
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MusicalAppContent(
+    modifier: Modifier = Modifier,
+    navigateToDetail: (Long) -> Unit,
+) {
+
+    val selectedDestination = remember { mutableStateOf(MusicalRoute.READER) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        
+        CenterAlignedTopAppBar(
+            modifier = modifier.background(MaterialTheme.colorScheme.surface),
+            title = {
+                Text(
+                    text = selectedDestination.value,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Localized description"
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "Localized description"
+                    )
+                }
+            }
+        )
+
+        when(selectedDestination.value){
+            MusicalRoute.READER -> {
+                ReaderView(modifier = modifier.weight(1f))
+            }
+            MusicalRoute.PLAYLIST -> {
+                PlaylistView(modifier = modifier.weight(1f))
+            }
+            MusicalRoute.SETTINGS -> {
+                SettingsView(modifier = modifier.weight(1f))
+            }
+        }
+
+        NavigationBar(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            TOP_LEVEL_DESTINATIONS.forEach { replyDestination ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = replyDestination.selectedIcon,
+                            contentDescription = stringResource(id = replyDestination.iconTextId)
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = stringResource(replyDestination.iconTextId)
+                        )
+                    },
+                    selected = selectedDestination.value == replyDestination.route,
+                    onClick = { selectedDestination.value = replyDestination.route }
+                )
+            }
+        }
+    }
+}
+
+object MusicalRoute {
+    const val READER = "Reader"
+    const val PLAYLIST = "Playlist"
+    const val SETTINGS = "Settings"
+}
+
+data class ReplyTopLevelDestination(
+    val route: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val iconTextId: Int
+)
+
+val TOP_LEVEL_DESTINATIONS = listOf(
+    ReplyTopLevelDestination(
+        route = MusicalRoute.READER,
+        selectedIcon = Icons.Default.GraphicEq,
+        unselectedIcon = Icons.Default.GraphicEq,
+        iconTextId = R.string.tab_reader
+    ),
+    ReplyTopLevelDestination(
+        route = MusicalRoute.PLAYLIST,
+        selectedIcon = Icons.Default.LibraryMusic,
+        unselectedIcon = Icons.Default.LibraryMusic,
+        iconTextId = R.string.tab_playlist
+    ),
+    ReplyTopLevelDestination(
+        route = MusicalRoute.SETTINGS,
+        selectedIcon = Icons.Outlined.Settings,
+        unselectedIcon = Icons.Outlined.Settings,
+        iconTextId = R.string.tab_settings
+    )
+)
