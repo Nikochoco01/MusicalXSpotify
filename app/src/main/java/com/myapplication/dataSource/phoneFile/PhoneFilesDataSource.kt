7e3a9d4984236e7b3a435d07a9a8dd11feb65e2d
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-10-22 12:14:08 +0200

    Update PhoneFilesDataSource.kt

--- app/src/main/java/com/myapplication/dataSource/phoneFile/PhoneFilesDataSource.kt
+++ app/src/main/java/com/myapplication/dataSource/phoneFile/PhoneFilesDataSource.kt
@@ -5,9 +5,18 @@ import com.myapplication.model.Playlists
 
 object PhoneFilesDataSource {
     private val musics = arrayListOf(
-        Musics(1, "", "music1", "1er", "Android", "", ""),
-        Musics(2, "", "music2", "1er", "Android", "", ""),
-        Musics(3, "", "music3", "1er", "Android", "", "")
+        Musics(1, "", "music1", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(2, "", "music2", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(3, "", "music3", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(4, "", "music4", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(5, "", "music5", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(6, "", "music6", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(7, "", "music7", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(8, "", "music8", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(9, "", "music9", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(10, "", "music10", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(11, "", "music11", "1er", "Android", "20/10/2023", "04:00:00"),
+        Musics(12, "", "music12", "1er", "Android", "20/10/2023", "04:00:00")
     )
     private val playlists = Playlists(1, "", "Test playlist", musics)
 
