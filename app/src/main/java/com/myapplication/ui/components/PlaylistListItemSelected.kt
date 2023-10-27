e4d0fbf86d3e9ba898cc3d729aabec6c212e99d4
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-10-27 16:02:29 +0200

    adding playlist item list and item list selected

--- app/src/main/java/com/myapplication/ui/components/PlaylistListItemSelected.kt
+++ app/src/main/java/com/myapplication/ui/components/PlaylistListItemSelected.kt
@@ -4,13 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,10 +29,11 @@ import com.myapplication.R
 import com.myapplication.model.MusicalPlaylists
 
 @Composable
-fun PlaylistListItem(
+fun PlaylistListItemSelected(
     playlist: MusicalPlaylists,
     modifier: Modifier = Modifier
 ){
+    val checkedState = remember { mutableStateOf(false) }
     ElevatedCard(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 6.dp
@@ -38,9 +45,15 @@ fun PlaylistListItem(
 
             }
     ) {
-        Column(
-            modifier
-            .fillMaxSize(),
+        Row (modifier.fillMaxWidth().padding(8.dp),
+            horizontalArrangement = Arrangement.End){
+            Checkbox(
+                checked = checkedState.value,
+                onCheckedChange = { checkedState.value = it },
+                modifier.size(24.dp)
+            )
+        }
+        Column(modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally) {
             PlaylistPicture(drawableResource = R.drawable.picture_1, description = "playlist picture")
