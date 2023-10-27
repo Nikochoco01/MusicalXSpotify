c5cd1bbc85d9f7f2fbd26717314421dbd1c8483f
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-10-27 10:34:26 +0200

    adding new Spotifyplayslist + rename Playlist to MuscialPlaylist

---
+++ app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
@@ -0,0 +1,13 @@
+package com.myapplication.model
+
+data class MusicalPlaylists(
+    val id: Int,
+    val images: String,
+    val name: String,
+    val tracks: List<Musics>)
+
+data class SpotifyPlaylist(
+    val id: Int,
+    val images: String,
+    val name: String,
+    val tracks: String)
--- app/src/main/java/com/myapplication/model/Playlists.kt
+++
@@ -1,7 +0,0 @@
-package com.myapplication.model
-
-data class Playlists(
-    val id: Int,
-    val images: String,
-    val name: String,
-    val tracks: List<Musics>)
