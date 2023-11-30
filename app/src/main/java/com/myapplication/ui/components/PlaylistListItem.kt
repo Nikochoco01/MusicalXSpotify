fc8612c6436a8553edfa69887f52994e6d18cfc9
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-12-01 00:27:43 +0100

    Update PlaylistListItem.kt

--- app/src/main/java/com/myapplication/ui/components/PlaylistListItem.kt
+++ app/src/main/java/com/myapplication/ui/components/PlaylistListItem.kt
@@ -1,7 +1,5 @@
 package com.myapplication.ui.components
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,16 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.myapplication.R
 import com.myapplication.model.MusicalPlaylists
-import com.myapplication.navigation.MusicalBarRoute
-import com.myapplication.navigation.MusicalPlaylistDetails
+import com.myapplication.navigation.MusicalInternalAppRoute
 
 @Composable
 fun PlaylistListItem(
@@ -32,7 +26,6 @@ fun PlaylistListItem(
     navController: NavController,
     modifier: Modifier = Modifier
 ){
-    Log.e("PLAYLIST", playlist.id.toString())
     ElevatedCard(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 6.dp
@@ -41,7 +34,7 @@ fun PlaylistListItem(
             .size(width = 160.dp, height = 224.dp)
             .clip(RoundedCornerShape(12.dp))
             .clickable {
-                var route: String = "${MusicalPlaylistDetails.Music.route}"
+                var route: String = "${MusicalInternalAppRoute.LoadPlaylist.route}"
                     .replace("{playlistID}", "${playlist.id}")
                 navController.navigate(route)
             }
