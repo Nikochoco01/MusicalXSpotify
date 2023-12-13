436bfc616b8d419ba80477a6780948c999c96572
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-12-13 17:24:08 +0100

    Update MusicalDatabaseSource.kt

--- app/src/main/java/com/myapplication/dataSource/database/MusicalDatabaseSource.kt
+++ app/src/main/java/com/myapplication/dataSource/database/MusicalDatabaseSource.kt
@@ -3,22 +3,22 @@ package com.myapplication.dataSource.database
 import com.myapplication.model.users.MusicalUsers
 
 object MusicalDatabaseSource {
-    suspend fun insertNewUser(users: MusicalUsers){
+    fun insertNewUser(users: MusicalUsers){
         MusicalDatabase.getInstance().musicalUsersDAO().insertNewUser(users)
     }
-    suspend fun updateExistingUser(users: MusicalUsers){
+    fun updateExistingUser(users: MusicalUsers){
         MusicalDatabase.getInstance().musicalUsersDAO().updateExistingUser(users)
     }
-    suspend fun deleteExistingUser(users: MusicalUsers){
+    fun deleteExistingUser(users: MusicalUsers){
         MusicalDatabase.getInstance().musicalUsersDAO().deleteExistingUser(users)
     }
-    suspend fun getExitingUserByCredentials(email: String , password: String){
-        MusicalDatabase.getInstance().musicalUsersDAO().getExitingUserByCredentials(email , password)
+    fun getExitingUserByCredentials(email: String , password: String): MusicalUsers?{
+       return MusicalDatabase.getInstance().musicalUsersDAO().getExitingUserByCredentials(email , password)
     }
-    suspend fun getExistingUserByMusicalUserID(usersID: Int){
-        MusicalDatabase.getInstance().musicalUsersDAO().getExistingUserByMusicalUserID(usersID)
+    fun getExistingUserByMusicalUserID(usersID: Int): MusicalUsers?{
+        return MusicalDatabase.getInstance().musicalUsersDAO().getExistingUserByMusicalUserID(usersID)
     }
-    suspend fun getExistingUserBySpotifyUserID(spotifyID: Int){
-        MusicalDatabase.getInstance().musicalUsersDAO().getExistingUserBySpotifyUserID(spotifyID)
+    fun getExistingUserBySpotifyUserID(spotifyID: Int): MusicalUsers?{
+        return MusicalDatabase.getInstance().musicalUsersDAO().getExistingUserBySpotifyUserID(spotifyID)
     }
 }
