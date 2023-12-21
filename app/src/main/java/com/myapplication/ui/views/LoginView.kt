7c2e4e09a6f3f9ef62767024ff285bebae147a90
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-12-21 08:35:35 +0100

    remove import

--- app/src/main/java/com/myapplication/ui/views/LoginView.kt
+++ app/src/main/java/com/myapplication/ui/views/LoginView.kt
@@ -1,6 +1,5 @@
 package com.myapplication.ui.views
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,8 +23,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.myapplication.navigation.MusicalBarRoute
 import com.myapplication.navigation.MusicalInternalAppRoute
 import com.myapplication.ui.utils.MusicalIcons
 import com.myapplication.viewModels.UsersViewModel
