5f759b816c3bb22fc555f9f7ae31a348d9bbf085
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-12-22 09:55:00 +0100

    create settings view

--- app/src/main/java/com/myapplication/ui/views/SettingsView.kt
+++ app/src/main/java/com/myapplication/ui/views/SettingsView.kt
@@ -1,25 +1,69 @@
 package com.myapplication.ui.views
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.myapplication.R
+import com.myapplication.ui.components.MusicalSettingItem
+import com.myapplication.ui.utils.MusicalIcons
 
 @Composable
 fun SettingsView(
     modifier: Modifier = Modifier
 ){
     Column (
-        modifier.fillMaxSize()
+        modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ){
-        Text(text= "Test Settings")
+        MusicalSettingItem(modifier = modifier,
+            textItem = R.string.setting_account,
+            textColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            icon = MusicalIcons.iconArrowRight,
+            iconDescription = R.string.icon_arrow_right
+        )
+        MusicalSettingItem(modifier = modifier,
+            textItem = R.string.setting_account_spotify,
+            textColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            icon = MusicalIcons.iconArrowRight,
+            iconDescription = R.string.icon_arrow_right
+        )
+        MusicalSettingItem(modifier = modifier,
+            textItem = R.string.setting_bluetooth,
+            textColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            icon = MusicalIcons.iconArrowRight,
+            iconDescription = R.string.icon_arrow_right
+        )
+        if(isSystemInDarkTheme())
+            MusicalSettingItem(modifier = modifier,
+                textItem = R.string.setting_Light_mode,
+                textColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                icon = MusicalIcons.iconLightMode,
+                iconDescription = R.string.icon_light_mode
+            )
+        else
+            MusicalSettingItem(modifier = modifier,
+                textItem = R.string.setting_Dark_mode,
+                textColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                icon = MusicalIcons.iconDarkMode,
+                iconDescription = R.string.icon_dark_mode
+            )
+        MusicalSettingItem(modifier = modifier,
+            textItem = R.string.setting_Logout,
+            textColor = MaterialTheme.colorScheme.onErrorContainer,
+            containerColor = MaterialTheme.colorScheme.errorContainer,
+            icon = MusicalIcons.iconLogout,
+            iconDescription = R.string.icon_logout
+        )
     }
-}
-
-@Preview
-@Composable
-fun SettingsPreview(){
-    SettingsView()
 }
