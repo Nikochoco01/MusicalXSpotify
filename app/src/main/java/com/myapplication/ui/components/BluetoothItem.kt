36839ae7d31b21978be4e82698d6e33ed9075b65
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-12-23 18:55:54 +0100

    Create BluetoothItem.kt

---
+++ app/src/main/java/com/myapplication/ui/components/BluetoothItem.kt
@@ -0,0 +1,33 @@
+package com.myapplication.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BluetoothItem(
+	modifier: Modifier,
+	deviceName: String,
+	onClickable: () -> Unit
+){
+	ListItem(
+		modifier = modifier
+			.clip(RoundedCornerShape(16.dp))
+			.clickable { onClickable.invoke() },
+		headlineText = { Text(text = deviceName, color = MaterialTheme.colorScheme.inversePrimary) },
+		colors = ListItemDefaults.colors(
+			containerColor = MaterialTheme.colorScheme.onPrimaryContainer
+		)
+	)
+}
