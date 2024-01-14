575f7f0b0ce477dcb900e7399f5f2ca6fd7fb9b2
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-01-14 14:59:54 +0100

    Update PlaylistViewModel.kt

--- app/src/main/java/com/myapplication/viewModels/PlaylistViewModel.kt
+++ app/src/main/java/com/myapplication/viewModels/PlaylistViewModel.kt
@@ -1,5 +1,7 @@
 package com.myapplication.viewModels
 
+import android.content.Context
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,15 +9,34 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myapplication.model.MusicalPlaylists
 import com.myapplication.repository.playlists.PlaylistRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PlaylistViewModel : ViewModel(){
+    private var _createPlaylistLiveData: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+    val createPlaylistLiveData: LiveData<Boolean> = _createPlaylistLiveData
+
     private var _playlistByIdLiveData : MutableLiveData<MusicalPlaylists> = MutableLiveData<MusicalPlaylists>()
     val playlistByIdLiveData : LiveData<MusicalPlaylists> = _playlistByIdLiveData
 
     private var _allPlaylistsLiveData : MutableLiveData<List<MusicalPlaylists>> = MutableLiveData<List<MusicalPlaylists>>()
     val allPlaylistsLiveData : LiveData<List<MusicalPlaylists>> = _allPlaylistsLiveData
+
+    fun createPlaylist(context: Context, fileUri: Uri){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                PlaylistRepository.createPlaylist(context, fileUri)
+                    .catch {
+                        Log.e("playlist error", it.toString())
+                    }
+                    .collect{
+                        _createPlaylistLiveData.postValue(it)
+                    }
+            }
+        }
+    }
 
     fun fetchPlaylistByID(id: Int){
         viewModelScope.launch {
@@ -25,7 +46,6 @@ class PlaylistViewModel : ViewModel(){
                 }
                 .collect{
                     _playlistByIdLiveData.postValue(it)
-                    Log.e("Test Call" , "CALL Music");
                 }
         }
     }
