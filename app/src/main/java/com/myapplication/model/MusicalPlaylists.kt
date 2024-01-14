8c267ce8a98dc53a69c5a68ac425b6ad94bd34a4
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-01-14 14:58:03 +0100

    Update MusicalPlaylists.kt

--- app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
+++ app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
@@ -1,10 +1,10 @@
 package com.myapplication.model
 
 data class MusicalPlaylists(
-    val id: Int,
-    val images: String,
-    val name: String,
-    val tracks: List<Musics>
+	val id: Int,
+	val images: String,
+	val name: String,
+	var tracks: List<Musics>
 )
 
 data class SpotifyPlaylist(
