35ee721b0351d710a82781140af09813ffa5a902
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-01-14 14:59:37 +0100

    Update PlaylistRepository.kt

--- app/src/main/java/com/myapplication/repository/playlists/PlaylistRepository.kt
+++ app/src/main/java/com/myapplication/repository/playlists/PlaylistRepository.kt
@@ -1,11 +1,17 @@
 package com.myapplication.repository.playlists
 
+import android.content.Context
+import android.net.Uri
 import com.myapplication.dataSource.phoneFile.PhoneFilesDataSource
 import com.myapplication.model.MusicalPlaylists
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 object PlaylistRepository {
+
+    suspend fun createPlaylist(context: Context, fileUri: Uri): Flow<Boolean> = flow{
+        emit(PhoneFilesDataSource.createPlaylist(context, fileUri))
+    }
     suspend fun getPlaylistByID(id: Int) : Flow<MusicalPlaylists> = flow {
         emit(PhoneFilesDataSource.getPlaylistByID(id))
     }
