9e75d8ca2b11319b52be06d978e37595fae44026
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-02-12 09:22:23 +0100

    Update MusicalPlaylists.kt

--- app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
+++ app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
@@ -1,12 +1,15 @@
 package com.myapplication.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MusicalPlaylists(
     val id: Int,
     val images: String,
     val name: String,
     val tracks: List<Musics>
 )
-
+@Serializable
 data class SpotifyPlaylist(
     val id: Int,
     val images: String,
