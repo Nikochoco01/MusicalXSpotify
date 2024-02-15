d62ee36390621d682d9770fa304bb85415a61399
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-02-15 18:53:25 +0100

    Update MusicalInitApp.kt

--- app/src/main/java/com/myapplication/MusicalInitApp.kt
+++ app/src/main/java/com/myapplication/MusicalInitApp.kt
@@ -3,7 +3,9 @@ package com.myapplication
 import android.app.Application
 import com.myapplication.dataSource.database.MusicalDatabase
 import com.myapplication.repository.users.UserMusicalManager
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MusicalInitApp : Application() {
     override fun onCreate() {
         super.onCreate()
