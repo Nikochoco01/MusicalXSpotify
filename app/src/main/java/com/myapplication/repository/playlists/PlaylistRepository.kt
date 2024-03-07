b08e3670c3ae1f11228accb18f301f0739394373
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-03-07 20:24:38 +0100

    Update PlaylistRepository.kt

--- app/src/main/java/com/myapplication/repository/playlists/PlaylistRepository.kt
+++ app/src/main/java/com/myapplication/repository/playlists/PlaylistRepository.kt
@@ -3,19 +3,23 @@ package com.myapplication.repository.playlists
 import com.myapplication.dataSource.phoneFile.PhoneFilesDataSource
 import com.myapplication.dataSource.spotifyApi.NetworkDataSource
 import com.myapplication.model.MusicalPlaylists
+import com.myapplication.model.SpotifyResultPlaylist
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.Response
 
 object PlaylistRepository {
-    suspend fun getPlaylist(id: Int) : Flow<MusicalPlaylists> = flow {
+    suspend fun getPlaylistByID(id: Int) : Flow<MusicalPlaylists> = flow {
         emit(PhoneFilesDataSource.getPhonePlaylistByID(id))
     }
 
-    suspend fun getPlaylists(userId: String): Flow<List<MusicalPlaylists>> = flow {
+    suspend fun getAllPhonePlaylists(userId: String): Flow<List<MusicalPlaylists>> = flow {
         val playlistsGot: MutableList<MusicalPlaylists> = mutableListOf()
         PhoneFilesDataSource.getAllPhonePlaylists().forEach { playlists: MusicalPlaylists ->  playlistsGot.add(playlists) }
-//        NetworkDataSource.apiServiceCallAPi.getAllUsersPlaylists(userId)
-
         emit(playlistsGot)
+    }
+
+    suspend fun getAllSpotifyPlaylists(userId: String, authorization: String): Flow<Response<SpotifyResultPlaylist>> = flow{
+        emit(NetworkDataSource.apiServiceCallAPi.getAllUsersPlaylists(userId, authorization))
     }
 }
