a876ef16489a12a254578addf8197c119e30ae49
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-03-07 23:47:15 +0100

    adding new option to the playlist menu

--- app/src/main/java/com/myapplication/ui/components/MusicalMenu.kt
+++ app/src/main/java/com/myapplication/ui/components/MusicalMenu.kt
@@ -5,19 +5,22 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import com.myapplication.model.SpotifyPlaylist
 
 @Composable
 fun MusicalMenu(
 	expanded: MutableState<Boolean>,
 	onDismiss: () -> Unit,
 	removePlaylist: () -> Unit,
-	importPlaylist: () -> Unit
+	importPlaylist: () -> Unit,
+	findSpotifyPlaylist: () -> Unit
 ){
 	DropdownMenu(
 		expanded = expanded.value,
 		onDismissRequest = { onDismiss.invoke()},
 		content = {
 			DropdownMenuItem(text = { Text(text = "Import playlist")}, onClick = { importPlaylist.invoke() })
+			DropdownMenuItem(text = { Text(text = "Import Spotify playlist")}, onClick = { findSpotifyPlaylist.invoke() })
 			DropdownMenuItem(text = { Text(text = "Remove playlist")}, onClick = { removePlaylist.invoke() })
 		}
 	)
