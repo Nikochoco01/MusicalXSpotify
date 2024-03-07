ea53d677f471c667439b75774e682a024ece7bb0
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-03-07 20:24:21 +0100

    Update MusicalPlaylists.kt

--- app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
+++ app/src/main/java/com/myapplication/model/MusicalPlaylists.kt
@@ -1,15 +1,38 @@
 package com.myapplication.model
 
+import com.google.gson.annotations.SerializedName
+
 data class MusicalPlaylists(
-    val id: Int,
+    val id: String,
     val images: String,
     val name: String,
     val tracks: List<Musics>
 )
 
 data class SpotifyPlaylist(
-    val id: Int,
-    val images: String,
+    @SerializedName("id")
+    val id: String,
+    val images: List<PictureSpotify>? = null,
+    @SerializedName("name")
     val name: String,
-    val tracks: String
+    val tracks: SpotifyTracks
+)
+
+data class PictureSpotify(
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("url")
+    val url: String,
+    @SerializedName("width")
+    val width: Int
+)
+
+data class SpotifyTracks(
+    @SerializedName("href")
+    val href: String
+)
+
+data class SpotifyResultPlaylist(
+    @SerializedName("items")
+    val playlists: List<SpotifyPlaylist>
 )
