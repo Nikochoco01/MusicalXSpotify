f1d1c535f8e1b1689fdc99ef179850ff0d40f9e3
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2024-03-08 18:59:15 +0100

    Update PlaylistViewModel.kt

--- app/src/main/java/com/myapplication/viewModels/PlaylistViewModel.kt
+++ app/src/main/java/com/myapplication/viewModels/PlaylistViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myapplication.model.MusicalPlaylists
 import com.myapplication.model.SpotifyResultPlaylist
+import com.myapplication.model.SpotifyResultTracks
 import com.myapplication.repository.playlists.PlaylistRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
@@ -24,6 +25,9 @@ class PlaylistViewModel : ViewModel(){
 
     private var _spotifyResultPlaylistsLiveData : MutableLiveData<SpotifyResultPlaylist> = MutableLiveData<SpotifyResultPlaylist>()
     val spotifyResultPlaylists : LiveData<SpotifyResultPlaylist> = _spotifyResultPlaylistsLiveData
+
+    private var _spotifyResultTracksPlaylist : MutableLiveData<SpotifyResultTracks> = MutableLiveData<SpotifyResultTracks>()
+    val spotifyResultTracksPlaylist : LiveData<SpotifyResultTracks> = _spotifyResultTracksPlaylist
 
     private var _allPhonePlaylists : MutableLiveData<List<MusicalPlaylists>> = MutableLiveData<List<MusicalPlaylists>>()
     val allPhonePlaylists : LiveData<List<MusicalPlaylists>> = _allPhonePlaylists
@@ -71,11 +75,25 @@ class PlaylistViewModel : ViewModel(){
             var validToken = "Bearer $token"
             PlaylistRepository.getAllSpotifyPlaylists(userId, validToken)
                 .catch {
-                    Log.e("playlist error" , it.toString())
+                    Log.e("fetch playlists error" , it.toString())
                 }
                 .collect{
                     Log.e("error" , "Result success ${it.isSuccessful}  Body ${it.body()}")
                     _spotifyResultPlaylistsLiveData.postValue(it.body())
+                }
+        }
+    }
+
+    fun fetchAllTracksFromPlaylist(playlistId: String, token: String){
+        viewModelScope.launch {
+            var validToken = "Bearer $token"
+            PlaylistRepository.getAllTracksFromPlaylist(playlistId, validToken)
+                .catch {
+                    Log.e("fetch tracks error" , it.toString())
+                }
+                .collect{
+                    Log.e("error" , "Result success ${it.isSuccessful}  Body ${it.body()}")
+                    _spotifyResultTracksPlaylist.postValue(it.body())
                 }
         }
     }
